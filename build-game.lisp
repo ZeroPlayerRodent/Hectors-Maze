e9f5281f0b2c30bd1b295746a46385c5c9b35b22
ZeroPlayerRodent <95110394+ZeroPlayerRodent@users.noreply.github.com>
2024-05-22 20:49:07 -0300

    Create new build script

--- build-game.lisp
+++ build-game.lisp
@@ -1,2 +1,4 @@
-(load "hectors-maze.asd")
-(asdf:make :hectors-maze)
+(asdf:load-system :sketch)
+(asdf:load-system :harmony)
+(load "hectors-maze.lisp")
+(sb-ext:save-lisp-and-die "hectors-maze" :toplevel #'mazegame:main :executable t :compression t)
